89e5e59a376e237f1add560d531f1e02e261bd13
lizjaimes <121306146+lizjaimes@users.noreply.github.com>
2024-03-06 13:01:30 -0600

    Add files via upload

---
+++ 11-todo-list/11-todo-list.js
@@ -0,0 +1,50 @@
+//create an array to store our to-do's, when we click add we want to add it to our array and then console.log our array
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}];
+renderTodoList();
+
+//created HTML with JavaScript
+function renderTodoList() {
+  let todoListHTML = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+   // const name = todoObject.name;
+   // const dueDate = todoObject.dueDate;
+   const {name, dueDate} = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="todoList.splice(${i}, 1);
+      renderTodoList();" class="delete-todo-button">Delete</button>
+      `;
+    todoListHTML += html;
+  }
+  // console.log(todoListHTML);
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+
+  }
+
+function addTodo () {
+   const inputElement = document.querySelector('.js-name-input');
+   const name = inputElement.value;
+
+   const dateInputElement = document.querySelector('.js-due-date-input');
+
+   const dueDate = dateInputElement.value;
+
+  // console.log(name);
+    todoList.push({
+      //name: name, dueDate: dueDate
+      name,
+      dueDate});
+    console.log(todoList);
+
+    //THIS WILL RESET THE TEXTBOX AFTER ADDING AN ITEM TO THE ARRAY
+    inputElement.value = '';
+
+   renderTodoList();
+
+}
